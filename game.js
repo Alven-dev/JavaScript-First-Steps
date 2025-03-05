bb7896b6d25f69c54502524f30d94d4222048233
Alven-dev <mikolajct@gmail.com>
2025-03-05 16:24:28 +0100

    More JS features + exercises

--- game.js
+++ game.js
@@ -1,6 +1,43 @@
-alert("Wild Pikachu appears!")
+alert("Young trainer! Welcome to pokemon adventure simulation!");
+alert("First you'll need a pokemon!");
 
-let trainer = prompt("How much HP does your pokemon have?")
+let starters = ["Placeholder", "Bulbasaur", "Charmander", "Squirtle"];
+
+alert("If you want to start a game with " + starters[1] + " type 1.");
+alert("If you want to start a game with " + starters[2] + " type 2.");
+alert("If you want to start a game with " + starters[3] + " type 3.");
+
+let choice = prompt("Which starter would you like to start with?");
+
+choice = parseInt(choice);
+
+if (choice >= 1 && choice <= 3) {
+    alert("Great choice! You selected " + starters[choice] + " as your starter Pokémon!");
+} else {
+    alert("Invalid choice! Please refresh and try again.");
+}
+
+let playerPokemon {
+    name: starters[choice],
+    health: 20,
+    level: 5,
+    experience: 0,
+    attack: function() {
+        if (this.name === "Charmander") {
+            return Math.floor
+        }
+    }
+}
+
+alert("Let's test your new Pokemon in a battle!");
+
+alert("A wild Pikachu appeared!")
+
+
+
+let trainer = prompt("How much HP does your pokemon have?");
+
+
 
 if (trainer > 50) {
     alert("Critical Hit!");
--- script.js
+++ script.js
@@ -48,3 +48,93 @@ if (note >= 10 === true || bonusProject === true) {
 } else {
     console.log("I'm sorry you didn't make it.");
 }
+
+
+// For example 1 //
+
+for (let hp = 1; hp <= 5; hp++) {
+    console.log("HP " + hp);
+}
+
+
+// While example 1 //
+
+let lives = 3;
+
+while (lives > 0) {
+    console.log("You have " + lives + " lives. Keep playing!!")
+    lives--;
+}
+
+console.log("GG");
+
+// Break example 1 //
+
+let compteur = 1
+
+while (true) {
+    console.log("Your number " + compteur);
+
+    if (compteur === 10){
+        console.log("STOP");
+        break;
+    }
+
+    compteur ++;
+}
+
+// forEach example 1 //
+
+let friends = ["Yasuo", "Garen", "Miss Fortune", "Nami", "Nocturne"];
+
+friends.forEach(function(nom) {
+    console.log("Welcome to Summoner's Rift " + nom +"!");
+});
+
+// Exercises (french) //
+
+for (let vienoiserie = 1; vienoiserie <= 10; vienoiserie++) {
+    console.log("Vienoiserie numero " + vienoiserie);
+}
+
+let croissants = 10;
+while (croissants > 0) {
+    console.log("Tu as toujours " + croissants + ", continues à manger!")
+    croissants --;
+}
+
+let vienoiseries = ["Pain aux chocolat", "Croissant", "Chausson aux pommes", "Brioche"];
+
+vienoiseries.forEach(function(nom) {
+    console.log("Aujourd'hui on a : " + nom +".");
+});
+
+// Game exercise //
+
+alert("You start your training!")
+
+for (let train = 1; train <= 10; train++) {
+    console.log("Training number " + train);
+}
+
+alert("Training completed!")
+
+alert("In front of you there are enemies. Your combat begins!")
+
+let stamina = 10;
+while (stamina > 0) {
+    alert("You are attacking...");
+    console.log("Ther are still " + stamina + " enemy(ies)");
+    stamina --;
+}
+
+alert("You defeated all enemies!")
+
+alert("You found a tressure!")
+
+let items = ["Frostmourne", "Ashbringer", "Tyrael's Might"];
+items.forEach (function(name) {
+    alert("Congratulations! You've found " + name +"!")
+});
+
+alert("You've run out of stamina, you need to rest")
